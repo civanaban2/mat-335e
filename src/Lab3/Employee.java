82b36d60ba2edcd1d52cdc8aca8d9b2a2a0c51f7
Civan Rumet Arı <urmet@Civan-MacBook-Air.local>
2024-11-01 15:39:12 +0300

    Initial commit

---
+++ src/Lab3/Employee.java
@@ -0,0 +1,53 @@
+package Lab3;
+
+@SuppressWarnings("unused")
+public class Employee {
+	
+	private String essn;
+	private double pricebyHour;
+	private int workingYear;
+	private boolean isRetired;
+
+	public Employee (String essn, int workingYear, double pricebyHour, boolean isRetired) {
+		this.essn = essn;
+		setWorkingYear(workingYear);
+		this.pricebyHour = pricebyHour;
+		setIsRetired(isRetired);
+	}
+
+	public int getWorkingYear() {
+		return workingYear;
+	}
+
+	public boolean getIsRetired() {
+		return isRetired;
+	}
+
+	public void setIsRetired(boolean isRetired) {
+		this.isRetired = isRetired;
+	}
+
+	public void setWorkingYear(int workingYear) {
+		this.workingYear = workingYear;
+		Retiring();
+	}
+
+	public boolean deserveBonus() {
+		if (workingYear >= 10 && !isRetired) {
+			return true;
+		}
+		else
+			return false;	
+	}
+
+	public double calculateSalary(int workingHour, int workingDays) {
+		double salary = workingHour * workingDays * pricebyHour;
+		return salary;
+	}
+
+	public void Retiring() {
+		if (workingYear >= 20) {
+			setIsRetired(true);
+		}
+	}
+}
